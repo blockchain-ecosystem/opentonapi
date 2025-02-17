e1f1a44aa1fb2b3677318a23f44835b9bedc29c8
lengoclinh-dev0608 <lnlinh0000@gmail.com>
2025-02-17 16:19:14 +0700

    logs

--- pkg/litestorage/litestorage.go
+++ pkg/litestorage/litestorage.go
@@ -1370,6 +1370,17 @@ func (s *LiteStorage) verifyBlockSequence(ctx context.Context, currentBlock tong
 		return err
 	}
 
+	s.logger.Info("initializing last processed seqno before verification",
+		zap.Uint32("lastProcessed", lastProcessed))
+
+	if lastProcessed == 0 {
+		lastProcessed = currentBlock.Seqno - 2
+		s.lastProcessedSeqno = currentBlock.Seqno - 2
+		s.logger.Info("initializing last processed seqno",
+			zap.Uint32("current_block", currentBlock.Seqno),
+			zap.Uint32("lastProcessed", lastProcessed))
+	}
+
 	// Check for gaps
 	if currentBlock.Seqno > lastProcessed+1 {
 		return s.recoverMissingBlocks(ctx, lastProcessed+1, currentBlock.Seqno-1)
